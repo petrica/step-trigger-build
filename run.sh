bfed8e61f058a95059cf3df2f2d5e15f908d0b16
Nick Humrich <nick.humrich@gmail.com>
2016-07-06 15:14:06 -0600

    Fix bash

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 WTB_ENDPOINT="https://app.wercker.com/api/v3/builds/
 
 
-WTB_JSON="{\"applicationId\": \"$WERCKER_TRIGGER_BUILD_APPLICATION_ID\", \
+WTB_JSON="\{\"applicationId\": \"$WERCKER_TRIGGER_BUILD_APPLICATION_ID\", \
 \"branch\": \"$WERCKER_GIT_BRANCH\", \
-\"message\": \"$WERCKER_TRIGGER_BUILD_MESSAGE\"}"
+\"message\": \"$WERCKER_TRIGGER_BUILD_MESSAGE\"\}"
+echo "$WTB_JSON"
 curl -s -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_WAIT_FOR_BUILD_TOKEN" "$WWFB_ENDPOINT" -d "$WTB_JSON"
 
