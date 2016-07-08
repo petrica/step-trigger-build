e00b0b6f376e41b8baa2eb73ab33105d8ecf134d
Nick Humrich <nick.humrich@gmail.com>
2016-07-08 10:10:03 -0600

    Update to use workflows api

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-WTB_ENDPOINT="https://app.wercker.com/api/v3/builds/"
+WTB_ENDPOINT="https://app.wercker.com/api/v3/runs/"
 
 
 WTB_JSON="{\"applicationId\": \"$WERCKER_TRIGGER_BUILD_APPLICATION_ID\", \
+\"pipelineId\": \"$WERCKER_TRIGGER_BUILD_PIPELINE_ID\", \
 \"branch\": \"$WERCKER_GIT_BRANCH\", \
-\"message\": \"$WERCKER_TRIGGER_BUILD_MESSAGE\"}"
+\"message\": \"$(git log -1 --pretty=%s)\"}"
 echo "$WTB_JSON"
+
 curl -s -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_TRIGGER_BUILD_TOKEN" "$WTB_ENDPOINT" -d "$WTB_JSON"
 
