3f8dfd24056c52276ca65d210810b64fc178336f
Petrica Martinescu <pmartinescu@tremend.ro>
2017-06-16 01:08:39 +0300

    Add source run it to curl

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ WTB_ENDPOINT="https://app.wercker.com/api/v3/runs/"
 WTB_JSON="{\"applicationId\": \"$WERCKER_TRIGGER_BUILD_APPLICATION_ID\", \
 \"pipelineId\": \"$WERCKER_TRIGGER_BUILD_PIPELINE_ID\", \
 \"branch\": \"$WERCKER_GIT_BRANCH\", \
+\"sourceRunId\": \"$WERCKER_RUN_ID\", \
 \"message\": \"$(git log -1 --pretty=%s)\"}"
 echo "$WTB_JSON"
 echo "Calling $WTB_ENDPOINT"
