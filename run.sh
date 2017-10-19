9c6f9b7337bf38f2250694ee1bd0c5006f4e659e
Petrica Martinescu <pmartinescu@gmail.com>
2017-10-19 18:53:11 +0300

    Fix curl

--- run.sh
+++ run.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-WTB_ENDPOINT="https://app.wercker.com/api/v3/runs/"
-
 if [ -z "$WERCKER_TRIGGER_BUILD_BRANCH" ]; then
   WERCKER_TRIGGER_BUILD_BRANCH="$WERCKER_GIT_BRANCH"
 fi
 
-WTB_JSON="{\"pipelineId\": \"$WERCKER_TRIGGER_BUILD_PIPELINE_ID\", \
-\"branch\": \"$WERCKER_TRIGGER_BUILD_BRANCH\", \
-\"sourceRunId\": \"$WERCKER_RUN_ID\", \
-\"message\": \"$(git log -1 --pretty=%s)\"}"
-echo "$WTB_JSON"
-echo "Calling $WTB_ENDPOINT"
+GIT_MESSAGE=$(git log -1 --pretty=%s)
 
-if ! curl --fail -k --write-out "\n\nStatus code: %{http_code}\n" -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_TRIGGER_BUILD_TOKEN" "$WTBC_ENDPOINT" -d "$WTBC_JSON"; then
+if [ "$WERCKER_TRIGGER_BUILD_MESSAGE" ]; then
+	GIT_MESSAGE="$WERCKER_TRIGGER_BUILD_MESSAGE"
+fi
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "pipelineId": "$WERCKER_TRIGGER_BUILD_PIPELINE_ID",
+  "branch": "$WERCKER_TRIGGER_BUILD_BRANCH",
+  "message": "$GIT_MESSAGE"
+}
+EOF
+}
+
+echo "$(generate_post_data)"
+
+if ! curl --fail -k --write-out "\n\nStatus code: %{http_code}\n" -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_TRIGGER_BUILD_TOKEN" -d "$(generate_post_data)" https://app.wercker.com/api/v3/runs/; then
   fail "$WBTC_TRIGGER_RESPONSE"
 fi
 
